2da78136550f97f3ebaf47daf8890d3342c00c5d
benjamin-olivera <benjamin.oliveracolonio@gmail.com>
2024-06-14 00:02:37 -0500

    ending the code

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,13 +1,15 @@
 
+import art
+import random
+
+print(art.logo)
 print("Welcome to the Number Guess Game!")
 print("I'm thinking of a number between 1 and 100.")
-
-import random
 numbers = range(1,101)
 number=random.choice(numbers)
 print(f"Psst, the correct answer is {number}")
-print("Choose a difficulty. Type esay or hard)")
-input_difficulty = input().toLowerCase()
+print("Choose a difficulty. Type easy or hard")
+input_difficulty = input().lower()
 if input_difficulty == "easy":
   attempts = 10
 else:
@@ -15,11 +17,18 @@ else:
 
 while attempts > 0:
     print("Make a guess.")
-    print(f"You have{attempts} attempts remaining to guess the number.")
-    guess = input("Enter your guess: ")
+    print(f"You have {attempts} attempts remaining to guess the number.")
+    guess = int(input("Enter your guess: "))
     if guess == number:
-       print("You got it! The answer was {number}.")
+       print(f"You got it! The answer was {number}.")
+       break
     elif guess < number:
-       print("Too low.")
+      print("Too low.")
     else :
-        print("Too high.")
+      print("Too high.")
+    attempts -= 1
+    print(f"Attempts remaining: {attempts}")
+
+if attempts == 0:
+    print("You've run out of guesses, you lose.")
+    print(f"The correct answer was {number}.")
